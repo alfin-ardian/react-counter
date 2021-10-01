2664dbaf0d6e1fc16801c94e02c4d5a75a3a5e07
alfin ardian <developer.alfin@gmail.com>
2021-10-01 21:00:11 +0700

    menambah navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+
+import ShoppingIcon from './assets/shopping-icon.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <nav className="nav">
+      <img className="nav-icon" src={ShoppingIcon} alt="Gambar Logo"/>
+      <h1 className="nav-title">Shopping List</h1>
+    </nav>
+    <section className="container">
+      
+    </section>
+  </>
   );
 }
 
