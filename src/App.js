aa88da8c3f839a4dadef9ab76fc89914decb9d1b
alfin ardian <developer.alfin@gmail.com>
2021-10-04 21:47:36 +0700

    part 15 & 16 - create todos state and todos divs

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import { useEffect,useState } from 'react';
 import './App.css';
 
 import ShoppingIcon from './assets/shopping-icon.svg'
+import MinusIcon from './assets/minus-icon.svg'
+import PlusIcon from './assets/plus-icon.svg'
 
 function App() {
   const [value, setValue] = useState('')
+  const [todos, setTodos] = useState([
+    {title: 'Telur', count: 1},
+    {title: 'Ayam', count: 1},
+    {title: 'Mie Instan', count: 1}
+  ])
   return (
   <>
     <nav className="nav">
@@ -16,6 +23,24 @@ function App() {
         <input className="input" value={value} onChange={(e) => setValue(e.target.value)} type="text" placeholder="silakan masukan list"/>
         <button className="add-button" type="submit">add</button>
       </form>
+
+      {todos.length > 0 ? (<div className="todos">{todos.map((todo, index) => {
+        return(
+          <div className="todo" key={index}>
+            {todo.title}
+            
+            <div className="todo-icon-wrapper">
+              <div className="todo-count">{todo.count}</div>
+              <button className="todo-action-button">
+                <img src={PlusIcon} alt="plus icon"/>
+              </button>
+              <button className="todo-action-button">
+                <img src={MinusIcon} alt="minus icon"/>
+              </button>
+            </div>
+          </div>
+        )
+      })}</div>):(<div>kosong</div>)}
     </section>
   </>
   );
